718f6c3bdbe3a3a80bf560c36c7475aafa8fa8bd
fathonymaulana <134576423+fathonymaulana@users.noreply.github.com>
2023-06-03 05:15:55 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// HYPERPLEXED
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.querySelector("li").onmouseover = event => {
+    let iterations = 0;
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if(index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+                    return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+                if(iterations >= event.target.dataset.value.length) {
+                clearInterval(interval);
+            }      
+        iterations += 1 / 3;
+    }, 30);
+
+}
+
+//Reset scroll top
+history.scrollRestoration = "manual";
+
+$(window).on('beforeunload', function(){
+      $(window).scrollTop(0);
+});
